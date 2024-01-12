05da5fb18604047bfc8c07da1391a42bbe8c97bd
Boulay Remi <o22205026@info22-11.iut45.univ-orleans.fr>
2024-01-12 13:42:56 +0100

    ajout de la page connection et d'inscription et modification du bouton se connecter par une icone

--- Flask/files/views.py
+++ Flask/files/views.py
@@ -5,3 +5,19 @@ from flask import render_template, flash, redirect, url_for, request
 def home():
     return render_template("home.html", title="Home")
 
+
+@app.route("/billeterie")
+def billeterie():
+    return render_template("billeterie.html") 
+
+@app.route("/programmation")
+def programmation():
+    return render_template("programmation.html") 
+
+@app.route("/connexion")
+def connexion():
+    return render_template("connexion.html")
+
+@app.route("/inscription")
+def inscription():
+    return render_template("inscription.html")
